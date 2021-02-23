cb989a2891d55fc592c80fa785edea6f754e6f51
dingeHCHS <dalton.inge@henderson.kyschools.us>
2021-02-23 08:53:46 -0600

    sum

---
+++ data.js
@@ -0,0 +1,49 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-accounting",
+      "name": "Accounting",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 3.109609926366205,
+        "pitch": 0.026812970445151407,
+        "fov": 1.4247463008564816
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -3.1341785481178057,
+          "pitch": 0.06296414345017354,
+          "title": "Colonel's Corner Bank",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
